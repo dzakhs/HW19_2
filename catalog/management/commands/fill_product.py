b8d2aa7c4fb9f598782698df52f63a50d68ea32b
Dmitry Zakhs <d.zakhs@gmail.com>
2023-06-06 00:28:40 +0300

    images works

--- catalog/management/commands/fill_product.py
+++ catalog/management/commands/fill_product.py
@@ -6,10 +6,10 @@ from catalog.models import Product
 class Command(BaseCommand):
     def handle(self, *args, **options):
         product_list= [
-            {"name":"LG", "description": "Двухкамерный холодильник", "image": "media/Lg.jpg", "category_id":5, "price": 20000},
-            {"name": "Liebherr", "description": "Двухкамерный холодильник с вместительной морозильной камерой", "image":"media/liebherr.jpg", "category_id":5, "price": 40000},
-            {"name": "Saeco", "description": "Кофемашина с предустановленными настройками", "image":"media/Saeco.jpg", "category_id":7, "price": 30000},
-            {"name": "Iphone 13 Pro Max", "description": "Смартфон с объемом памяти 256гб", "image": "media/Iphone.jpg", "category_id":1, "price": 100000 }
+            {"name": "LG", "description": "Двухкамерный холодильник", "image": "product/media/Lg.jpg", "category_id":5, "price": 20000},
+            {"name": "Liebherr", "description": "Двухкамерный холодильник с вместительной морозильной камерой", "image":"product/media/liebherr.jpg", "category_id":5, "price": 40000},
+            {"name": "Saeco", "description": "Кофемашина с предустановленными настройками", "image":"product/media/Saeco.jpg", "category_id":7, "price": 30000},
+            {"name": "Iphone 13 Pro Max", "description": "Смартфон с объемом памяти 256гб", "image": "product/media/Iphone.jpg", "category_id":1, "price": 100000 }
 
         ]
         product_for_create = []
--- catalog/templatetags/mytags.py
+++ catalog/templatetags/mytags.py
@@ -1,12 +1,16 @@
+import os
 
 from django import template
 
+from HW19_2 import settings
 
 register = template.Library()
 
 @register.simple_tag(name='media_path')
 def mediapath(image_path):
-    return f"/media/{image_path}"
+    media_url = settings.MEDIA_URL
+    full_path = os.path.join(media_url, str(image_path))
+    return full_path
 
 
 #@register.filter(name='media_path')
